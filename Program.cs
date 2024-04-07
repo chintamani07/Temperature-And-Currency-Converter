0dfe44da2253605844e805a57e1ca69b82516045
Chintamani Vijaykumar Wankhede <125362482+chintamani07@users.noreply.github.com>
2024-04-07 20:17:12 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,7 @@ namespace TemperatureConverter
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Welcome to Currency Converter System ****");
             Console.WriteLine("Welcome ! Please enter 'c' to use Currency Converter or anything else to use the Temperature Converter.");
             
             string operation = Console.ReadLine();
@@ -17,11 +18,11 @@ namespace TemperatureConverter
             if(operation == "c")
             {
                 Console.WriteLine("You have selected the Currency Converter Operation!");
-                Console.WriteLine("Please enter 'a' to Convert from Pounds or anything else to convert from Euros.");
+                Console.WriteLine("Please enter 'a' to Convert Currency from Pounds or anything else to convert from Euros.");
                 
-                string decdision = Console.ReadLine();
+                string decision = Console.ReadLine();
 
-                if(decdision == "a")
+                if(decision == "a")
                 {
                     Console.WriteLine("Please enter your amount in Pounds : ");
                     float pounds = float.Parse(Console.ReadLine());
@@ -34,6 +35,31 @@ namespace TemperatureConverter
                     float euros = float.Parse(Console.ReadLine());
                     float pounds = euros / 1.16f;
                     Console.WriteLine("The amount in Pounds is : " + pounds.ToString("0.00"));
+                }
+            }
+            else
+            {
+                Console.WriteLine("***** Welcome to Temperature Converter System ****");
+                Console.WriteLine("You have selected the Temperature Converter Operation!");
+                Console.WriteLine("Please enter 'a' to Convert Temperature from Celcius or anything else to convert from Euros.");
+
+                string decision = Console.ReadLine();
+
+                if (decision == "a")
+                {
+                    Console.WriteLine("Please enter value in Celcius : ");
+                    float celcius = float.Parse(Console.ReadLine());
+                    float farhenhite = (float)(celcius * 1.8 + 32f);
+                    Console.WriteLine("The temperature in Farhenhite is : " + farhenhite);
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("Please enter value in Farhenhite : ");
+                    float farhenhite = float.Parse(Console.ReadLine());
+                    float celcius = (farhenhite - 32f) / 1.8f;
+                    Console.WriteLine("The temperature in Celcius is : " + celcius);
+                    Console.ReadKey();
                 }
             }
         }    
